4c550591ecb93df4c898b4d5397de78e1c1f3403
Dustin Stacy <dustin.stacy@outlook.com>
2023-11-08 10:34:28 -0500

    Simplify contract

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -5,16 +5,36 @@ pragma solidity ^0.8.19;
 /// Custom Errors ///
 /////////////////////
 
+error CoinFlip__NotEnoughWagered(uint256 minimum, uint256 sent);
+
 contract CoinFlip {
-    uint256 private _minimumWager;
+    uint256 private immutable _minimumWager;
+    uint256 private coinFlipResult;
+    uint256 private lastTimeStamp;
 
-    mapping(address => uint256) private _balances;
-
-    //////////////
-    /// Events ///
-    //////////////
+    event WagerEntered(
+        address indexed entrant,
+        uint256 entrantsWager,
+        uint256 entrantsGuess
+    );
 
     constructor(uint256 minimumWager_) {
         _minimumWager = minimumWager_;
     }
+
+    //////////////////////
+    /// Main Functions ///
+    //////////////////////
+
+    function enterWager(uint256 entrantsGuess) public payable {
+        if (msg.value < _minimumWager) {
+            revert CoinFlip__NotEnoughWagered(_minimumWager, msg.value);
+        }
+        lastTimeStamp = block.timestamp;
+        coinFlipResult = lastTimeStamp % 2;
+        emit WagerEntered(msg.sender, msg.value, entrantsGuess);
+        fulfillRandomCoinFlipResult();
+    }
+
+    function fulfillRandomCoinFlipResult() private {}
 }
